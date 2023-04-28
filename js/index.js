b2ca6c91c8e10b030de40083258ba80a6ca7827d
swimchicken <swimchickenOuO@gmail.com>
2023-04-29 01:39:52 +0800

    update -section

--- js/index.js
+++ js/index.js
@@ -13,3 +13,12 @@ const iconclose = document.querySelector(".icon-close");
 iconclose.onclick = () => {
     wrapper.classList.remove("active-popup");
 }
+function selectTitle(titleNum){
+    var titles = document.getElementsByTagName("h2");
+        for (var i = 0; i < titles.length; i++) {
+          titles[i].classList.remove("selected");
+        }
+        // 再將被點擊的標題設定為選中狀態
+        var selectedTitle = document.getElementById("title" + titleNum);
+        selectedTitle.classList.add("selected");
+}
