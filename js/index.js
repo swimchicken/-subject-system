564878a6dbc609bd32a25ce107cf8ecca416d3de
za86za86 <73220647+za86za86@users.noreply.github.com>
2023-05-05 14:29:33 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -14,11 +14,21 @@ iconclose.onclick = () => {
     wrapper.classList.remove("active-popup");
 }
 function selectTitle(titleNum){
-    var titles = document.getElementsByTagName("h2");
+    var titles = document.getElementsByTagName("h3");
+    var chats = document.getElementsByTagName("h4");
         for (var i = 0; i < titles.length; i++) {
           titles[i].classList.remove("selected");
+          titles[i].classList.add("unselected");
+          chats[i].classList.remove("choose_label");
+          chats[i].classList.add("unchoose_label");
         }
         // 再將被點擊的標題設定為選中狀態
         var selectedTitle = document.getElementById("title" + titleNum);
+        var chat2 = document.getElementById("chat" + titleNum);
         selectedTitle.classList.add("selected");
+        selectedTitle.classList.remove("unselected");
+        chat2.classList.add("choose_label");
+        chat2.classList.remove("unchoose_label");
 }
+
+
